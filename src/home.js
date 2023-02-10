efa8915ea3ae9bd1a5dbf09c7a82ff1b2aa766d6
Hvbert1 <henryjiafu@gmail.com>
2023-02-11 07:42:33 +1100

    create new module for homepage

---
+++ src/home.js
@@ -0,0 +1,22 @@
+function createHome() {
+    const heading = document.createElement("h1");
+    const main = document.createElement("div");
+    const mainHeading = document.createElement("div");
+    const mainContent = document.createElement("div");
+    const btn = document.createElement("button");
+
+    main.id = "main";
+    heading.innerHTML = "Gusteau's";
+    mainHeading.innerHTML = "About Us";
+    mainContent.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At consectetur lorem donec massa sapien faucibus et molestie ac. Nulla porttitor massa id neque aliquam vestibulum morbi blandit cursus. Purus ut faucibus pulvinar elementum integer enim neque volutpat ac. Tellus id interdum velit laoreet id donec ultrices tincidunt."
+    btn.innerHTML = "Reserve now";
+
+    document.getElementById("content").appendChild(heading);
+    document.getElementById("content").appendChild(main);
+    document.getElementById("content").appendChild(btn);
+
+    document.getElementById("main").appendChild(mainHeading);
+    document.getElementById("main").appendChild(mainContent);
+}
+
+export default createHome();
--- src/index.js
+++ src/index.js
@@ -1,23 +1,11 @@
-console.log('test');
 
-const heading = document.createElement("h1");
-const main = document.createElement("div");
-const mainHeading = document.createElement("div");
-const mainContent = document.createElement("div");
+import loadHome from './home'
 
-main.id = "main";
-heading.innerHTML = "Gusteau's";
-mainHeading.innerHTML = "About Us"
-mainContent.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At consectetur lorem donec massa sapien faucibus et molestie ac. Nulla porttitor massa id neque aliquam vestibulum morbi blandit cursus. Purus ut faucibus pulvinar elementum integer enim neque volutpat ac. Tellus id interdum velit laoreet id donec ultrices tincidunt."
+function createHome() {
+    loadHome();
+}
 
-document.getElementById("content").appendChild(heading);
-document.getElementById("content").appendChild(main);
-
-document.getElementById("main").appendChild(mainHeading);
-document.getElementById("main").appendChild(mainContent);
-
-
-
+createHome();
 
 
 
