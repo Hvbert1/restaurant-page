b0ded7535a95b6de6177cc71f874f5045a368f12
Hvbert1 <henryjiafu@gmail.com>
2023-02-13 11:17:33 +1100

    stylise homepage

--- src/home.js
+++ src/home.js
@@ -1,4 +1,5 @@
 function createHome() {
+    const header = document.createElement("div")
     const heading = document.createElement("h1");
     const main = document.createElement("div");
     const mainHeading = document.createElement("div");
@@ -6,19 +7,25 @@ function createHome() {
     const mainPic = document.createElement("img");
     const btn = document.createElement("button");
 
+    header.id = "header";
     main.id = "main";
-    heading.innerHTML = "Gusteau's";
+    mainHeading.id = "about"
+    mainPic.id = "logo"
+
+    heading.innerHTML = "Welcome to";
     mainHeading.innerHTML = "About Us";
     mainContent.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At consectetur lorem donec massa sapien faucibus et molestie ac. Nulla porttitor massa id neque aliquam vestibulum morbi blandit cursus. Purus ut faucibus pulvinar elementum integer enim neque volutpat ac. Tellus id interdum velit laoreet id donec ultrices tincidunt."
-    btn.innerHTML = "Reserve now";
+    btn.innerHTML = "Reservations";
 
-    document.getElementById("content").appendChild(heading);
+    document.getElementById("content").appendChild(header);
     document.getElementById("content").appendChild(main);
     document.getElementById("content").appendChild(btn);
 
     document.getElementById("main").appendChild(mainPic);
     document.getElementById("main").appendChild(mainHeading);
     document.getElementById("main").appendChild(mainContent);
+    document.getElementById("header").appendChild(heading);
+
 }
 
 function loadHome() {
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 
 import loadHome from './home';
 import './style.css';
+import Icon from './logo.png';
+
+
+const myIcon = new Image();
+myIcon.src = Icon;
+document.getElementById("header").appendChild(myIcon);
 
 loadHome();
 
