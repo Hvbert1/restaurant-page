26f00cc6e731a32222661b727f1c71c7fcfb4900
Hvbert1 <henryjiafu@gmail.com>
2023-02-13 11:32:38 +1100

    change description

--- src/home.js
+++ src/home.js
@@ -4,24 +4,22 @@ function createHome() {
     const main = document.createElement("div");
     const mainHeading = document.createElement("div");
     const mainContent = document.createElement("div");
-    const mainPic = document.createElement("img");
     const btn = document.createElement("button");
 
     header.id = "header";
     main.id = "main";
     mainHeading.id = "about"
-    mainPic.id = "logo"
 
     heading.innerHTML = "Welcome to";
     mainHeading.innerHTML = "About Us";
-    mainContent.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At consectetur lorem donec massa sapien faucibus et molestie ac. Nulla porttitor massa id neque aliquam vestibulum morbi blandit cursus. Purus ut faucibus pulvinar elementum integer enim neque volutpat ac. Tellus id interdum velit laoreet id donec ultrices tincidunt."
+    mainContent.innerHTML = "Gusteau's is a fine dining restaurant located in the heart of Paris, home to the most famous chef in Paris, Auguste Gusteau. The restaurant overlooks the Eiffel Tower and is a delight in both food and in experience."; 
+
     btn.innerHTML = "Reservations";
 
     document.getElementById("content").appendChild(header);
     document.getElementById("content").appendChild(main);
     document.getElementById("content").appendChild(btn);
 
-    document.getElementById("main").appendChild(mainPic);
     document.getElementById("main").appendChild(mainHeading);
     document.getElementById("main").appendChild(mainContent);
     document.getElementById("header").appendChild(heading);
